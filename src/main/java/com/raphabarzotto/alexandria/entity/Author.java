0f61b7715836f1e000b4f5e9f81f062deeb3c04b
raphabarzotto <raphabarzotto@gmail.com>
2024-08-05 16:56:21 -0300

    feat: add Hibernate @Entity to entities and set Primary-Key

--- src/main/java/com/raphabarzotto/alexandria/entity/Author.java
+++ src/main/java/com/raphabarzotto/alexandria/entity/Author.java
@@ -1,7 +1,17 @@
 package com.raphabarzotto.alexandria.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authors")
 public class Author {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
